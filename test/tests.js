fa7939936109727c586d94db97b59f1408a7958f
Hernán Tierno <htierno@gmail.com>
2014-10-21 11:28:29 -0300

    Added unit tests for inner errors

--- test/tests.js
+++ test/tests.js
@@ -1,158 +1,219 @@
+/*global describe, it */
 require('../index.js');
 
 var assert = require('assert');
 
-describe("simple-errors", function() {
+describe("simple-errors", function () {
+    "use strict";
 
-	it("should add create factory to Error class", function () {
-		assert.ok(Error.create);
-	});
+    it("should add create factory to Error class", function () {
+        assert.ok(Error.create);
+    });
 
-	it("should add http factory to Error class", function () {
-		assert.ok(Error.http);
-	});
+    it("should add http factory to Error class", function () {
+        assert.ok(Error.http);
+    });
 
-	it("should add toJson helper method", function () {
-		assert.ok(Error.toJson);
-	});
+    it("should add toJson helper method", function () {
+        assert.ok(Error.toJson);
+    });
 
-	describe("create", function () {
+    describe("create", function () {
 
-		it("should return an Error instance", function () {
+        it("should return an Error instance", function () {
 
-			var err = Error.create('foo');
+            var err = Error.create('foo');
 
-			assert.ok(err instanceof Error);
-			assert.equal('foo', err.message);
-		});
+            assert.ok(err instanceof Error);
+            assert.equal('foo', err.message);
+        });
 
-		it("should use 'Unknown error' by default", function () {
+        it("should use 'Unknown error' by default", function () {
 
-			var err = Error.create();
+            var err = Error.create();
 
-			assert.equal('Unknown error', err.message);
-		});
+            assert.equal('Unknown error', err.message);
+        });
 
-		it("should accept a string as data", function () {
+        it("should accept a string as data", function () {
 
-			var err = Error.create('foo', 'bar');
+            var err = Error.create('foo', 'bar');
 
-			assert.equal('bar', err.data);
-		});
+            assert.equal('bar', err.data);
+        });
 
-		it("should accept another Error as inner", function () {
+        it("should accept another Error as inner", function () {
 
-			var err = Error.create('foo', new Error('bar'));
+            var err = Error.create('foo', new Error('bar'));
 
-			assert.equal(undefined, err.data);
-			assert.ok(err.inner instanceof Error);
-		});
+            assert.equal(undefined, err.data);
+            assert.ok(err.inner instanceof Error);
+        });
 
-		it("should add properties defined in data", function () {
+        it("should add properties defined in data", function () {
 
-			var err = Error.create('foo', {status: 200});
+            var err = Error.create('foo', {status: 200});
 
-			assert.equal(200, err.status);
-		});
+            assert.equal(200, err.status);
+        });
 
-		it("should accept an inner error in string format", function () {
+        it("should accept an inner error in string format", function () {
 
-			var err = Error.create('foo', {}, 'boo');
+            var err = Error.create('foo', {}, 'boo');
 
-			assert.equal('boo', err.inner);
-		});
-	});
+            assert.equal('boo', err.inner);
+        });
+    });
 
-	describe("http", function () {
+    describe("http", function () {
 
-		it("should use 500 as default", function () {
+        it("should use 500 as default", function () {
 
-			var err = Error.http();
+            var err = Error.http();
 
-			assert.equal('Internal Server Error', err.message);
-			assert.equal(500, err.status);
-		});
+            assert.equal('Internal Server Error', err.message);
+            assert.equal(500, err.status);
+        });
 
-		it("should use 500 as default", function () {
+        it("should use 500 as default", function () {
 
-			var err = Error.http('foo');
+            var err = Error.http('foo');
 
-			assert.equal('foo', err.message);
-			assert.equal(500, err.status);
-		});
+            assert.equal('foo', err.message);
+            assert.equal(500, err.status);
+        });
 
-		it("should add a default description", function () {
+        it("should add a default description", function () {
 
-			var err = Error.http(400);
+            var err = Error.http(400);
 
-			assert.equal('Bad Request', err.message);
-			assert.equal(400, err.status);
-		});
+            assert.equal('Bad Request', err.message);
+            assert.equal(400, err.status);
+        });
 
-		it("should add string data to error", function () {
+        it("should add string data to error", function () {
 
-			var err = Error.http(400, "foo", "bar");
+            var err = Error.http(400, "foo", "bar");
 
-			assert.equal('foo', err.message);
-			assert.equal('bar', err.data);
-			assert.equal(400, err.status);
-		});
+            assert.equal('foo', err.message);
+            assert.equal('bar', err.data);
+            assert.equal(400, err.status);
+        });
 
-		it("should add object data to error", function () {
+        it("should add object data to error", function () {
 
-			var err = Error.http(400, "foo", {x: "bar"});
+            var err = Error.http(400, "foo", {x: "bar"});
 
-			assert.equal('foo', err.message);
-			assert.equal('bar', err.x);
-			assert.equal(400, err.status);
-		});
+            assert.equal('foo', err.message);
+            assert.equal('bar', err.x);
+            assert.equal(400, err.status);
+        });
 
-		it("should add inner error", function () {
+        it("should add inner error", function () {
 
-			var err = Error.http(400, "foo", {}, "something wrong");
+            var err = Error.http(400, "foo", {}, "something wrong");
 
-			assert.equal('foo', err.message);
-			assert.equal('something wrong', err.inner);
-			assert.equal(400, err.status);
-		});
-	});
+            assert.equal('foo', err.message);
+            assert.equal('something wrong', err.inner);
+            assert.equal(400, err.status);
+        });
+    });
 
-	describe("toJson", function () {
+    describe("toJson", function () {
 
-		it("should return an object with all the properties", function () {
-			var err = Error.create('foo', { foo: 'bar' }, 'boo'),
-			obj = Error.toJson(err);
+        it("should return an object with all the properties", function () {
+            var err = Error.create('foo', { foo: 'bar' }, 'boo'),
+                obj = Error.toJson(err);
 
-			assert.ok(!(obj instanceof Error));
-			assert.equal('foo', obj.message);
-			assert.equal('bar', obj.foo);
-			assert.equal('boo', obj.inner);
-		});
+            assert.ok(!(obj instanceof Error));
+            assert.equal('foo', obj.message);
+            assert.equal('bar', obj.foo);
+            assert.equal('boo', obj.inner);
+        });
 
-		it("should have stack in the keys collection", function () {
-			var err = Error.create('foo', { foo: 'bar' }, 'boo'),
-			obj = Error.toJson(err);
+        it("should have stack in the keys collection", function () {
+            var err = Error.create('foo', { foo: 'bar' }, 'boo'),
+                obj = Error.toJson(err);
 
-			assert.ok(Object.keys(obj).indexOf('stack') >= 0);
-		});
+            assert.ok(Object.keys(obj).indexOf('stack') >= 0);
+        });
 
-		it("should recursively turn errors into literals", function () {
-			var err = Error.create('foo', {}, Error.create('bar'));
-			var obj = Error.toJson(err);
+        it("should recursively turn errors into literals", function () {
+            var err = Error.create('foo', {}, Error.create('bar')),
+                obj = Error.toJson(err);
 
-			assert.equal('bar', obj.inner.message);
-		});
+            assert.equal('bar', obj.inner.message);
+        });
 
-		it("should support a 'string' in inner", function () {
-			var err = Error.create('foo', {}, "something bad happened");
+        it("should support a 'string' in inner", function () {
+            var err = Error.create('foo', {}, "something bad happened");
 
-			assert.equal("something bad happened", Error.toJson(err).inner);
-		});
+            assert.equal("something bad happened", Error.toJson(err).inner);
+        });
 
-		it("should return non Error objects", function () {
-			var err = Error.toJson({foo: 'bar'});
+        it("should return non Error objects", function () {
+            var err = Error.toJson({foo: 'bar'});
 
-			assert.deepEqual({foo: 'bar'}, err);
-		});
-	});
+            assert.deepEqual({foo: 'bar'}, err);
+        });
+    });
+
+    describe("Inner errors", function () {
+        it("should set inner status code to new error", function () {
+            var innerError = Error.http(401, "You don't have access to the resource"),
+                error = Error.create("The username doesn't have access.", innerError);
+
+            assert.strictEqual(innerError.status, 401);
+            assert.strictEqual(error.status, 401);
+        });
+
+        it("should show public message only", function () {
+            var publicMessage = "My public Error message",
+                innerError = new Error("Test Inner error"),
+                error = Error.create(publicMessage, innerError);
+
+            assert.ok(error);
+            assert.equal(publicMessage, Error.publicMessage(error));
+        });
+
+        it("should show public message only without inner", function () {
+            var publicMessage = "My public Error message",
+                error = Error.create(publicMessage);
+
+            assert.ok(error);
+            assert.equal(publicMessage, Error.publicMessage(error));
+        });
+
+        it("should not show any message", function () {
+            var publicMessage = "My public Error message",
+                error = Error.create(publicMessage, {public: false});
+
+            assert.ok(error);
+            assert.equal("", Error.publicMessage(error));
+        });
+
+        it("should show every public error message", function () {
+            var publicMessage1 = "The table name is incorrect",
+                publicMessage2 = "The User can't be created. Please try again with a different name",
+                internalInnerError = new Error("Error in DB query. Table 'test' does not exists"),
+                dbError = new Error(internalInnerError),
+                publicError1 = Error.create(publicMessage1, dbError),
+                publicError2 = Error.create(publicMessage2, null, publicError1);
+
+            assert.ok(publicError2);
+            assert.equal(publicMessage2 + " - " + publicMessage1, Error.publicMessage(publicError2));
+        });
+
+        it("should not show a private message", function () {
+            var publicMessage1 = "The table 'users' is invalid",
+                publicMessage2 = "The User can't be created. Please try again with a different name",
+                internalInnerError = new Error("Error in DB query. Table 'test' does not exists"),
+                dbError = new Error(internalInnerError),
+                publicError1 = Error.create(publicMessage1, {public: false}, dbError),
+                publicError2 = Error.create(publicMessage2, null, publicError1);
+
+            assert.ok(publicError2);
+            assert.equal(publicMessage2, Error.publicMessage(publicError2));
+        });
+    });
 });
